2fc7e58fdbc5a114353d2c3b6bf04f4713c582d9
OGxploit <i.k.krijezi@gmail.com>
2025-06-14 23:42:57 +0200

    Update main.py to add DNS lookup support

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import argparse
 from src.whois_lookup import whois_lookup
+from src.dns_lookup import dns_lookup
 
 def main():
     parser = argparse.ArgumentParser(description="Recon & Enumeration Toolkit")
     parser.add_argument('--whois', help='Perform WHOIS lookup on a domain')
+    parser.add_argument('--dns', help='Perform DNS lookup on a domain')
 
     args = parser.parse_args()
 
     if args.whois:
         result = whois_lookup(args.whois)
+        print(result)
+
+    if args.dns:
+        result = dns_lookup(args.dns)
         print(result)
 
 if __name__ == '__main__':
